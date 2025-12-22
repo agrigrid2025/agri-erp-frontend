25c452996e9cdbf9ec484e4187c269e00d7fadc4
agrigrid2025 <info@agrigrid.net>
2025-12-22 21:19:31 +0000

    Add Link

--- src/components/SupplierForm.jsx
+++ src/components/SupplierForm.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams, useNavigate, useLocation } from 'react-router-dom';
+import { useParams, useNavigate, useLocation, Link } from 'react-router-dom';
 
 export default function SupplierForm() {
   const { tenant } = useParams();
