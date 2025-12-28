95c3778e6b99be606b367aaf8d717fce93867dbc
agrigrid2025 <info@agrigrid.net>
2025-12-28 08:37:53 +0000

    Add Link to Receipt Form

--- src/components/ReceiptForm.jsx
+++ src/components/ReceiptForm.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 
 export default function ReceiptForm() {
   const { tenant, poId } = useParams();
