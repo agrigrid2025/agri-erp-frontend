4e0c1f54f4dd8880032eb588ea8529f0f87a5b1c
agrigrid2025 <info@agrigrid.net>
2026-01-03 22:41:00 +0000

    Update Error in EquipmentForm.jsx

--- src/components/EquipmentForm.jsx
+++ src/components/EquipmentForm.jsx
@@ -51,7 +51,7 @@ export default function EquipmentForm() {
             setFormData({
               name: eq.name || '',
               fleet_number: eq.fleet_number || '',
-              equipment_type: eq.type || '',
+              equipment_type: eq.type || '',  // This is the ID from API
               make: eq.make || '',
               model: eq.model || '',
               registration_number: eq.registration_number || '',
